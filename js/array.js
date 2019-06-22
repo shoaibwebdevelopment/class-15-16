412bd68a0a2e136a6155cd7edee057253d55ccca
Shoaib Ahned <shoaib1971@outlook.com>
2019-06-22 18:03:38 +0600

    This is comment.

---
+++ js/array.js
@@ -0,0 +1,7 @@
+ var category = ['Shirt', 'Pant', 'Dairy', 'Pajama', 'Longgi'];
+//  alert(category[0]);
+var length = category.length;
+alert(length);
+for(var i=0; i<length; i++){
+    document.write(category[i],"<br>");
+}
---
+++ js/table.js
@@ -0,0 +1,5 @@
+
+function createNewdata(){
+    alert("something");
+        
+    }
